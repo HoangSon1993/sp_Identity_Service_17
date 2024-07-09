91726809105bbfbd2a7133417450366b6c8d7a64
Hoang Son <son.lyhoang2014@gmail.com>
2024-07-10 00:07:18 +0700

    Isolation trong UnitTest, best practice trong Unit test

--- src/test/java/sondev/indentityservice/controller/UserControllerTest.java
+++ src/test/java/sondev/indentityservice/controller/UserControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -26,6 +27,7 @@ import java.util.Date;
 @AutoConfigureMockMvc
 @Slf4j
 @SpringBootTest //init framework spring boot (connect dbms, init beans)
+@TestPropertySource("/test.properties")
 public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
